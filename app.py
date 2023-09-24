d46ee9e61b58e522a0f99a6a2a04ef69d9acc22c
Shivansh Dwivedi <shivansh2283@gmail.com>
2023-09-24 12:54:17 +0530

    Added Translation

--- app.py
+++ app.py
@@ -2,10 +2,13 @@ import streamlit as st
 import openai
 import os
 from model import search
+from googletrans import Translator
 
 OPENAI_API_KEY = st.secrets['OPENAI_API_KEY']
 
 openai.api_key = OPENAI_API_KEY
+
+translator = Translator()
 
 st.set_page_config(page_title="Team Ocean Astronauts", page_icon="📁", layout="wide")
 
@@ -13,12 +16,6 @@ st.markdown(
     '<p style="display:inline-block;font-size:40px;font-weight:bold;">Team Ocean Astronauts</p> <p style="display:inline-block;font-size:16px;">With LlamaIndex&#39;s in-context learning approach, LlamaDoc leverages the reasoning capabilities of LLMs to provide accurate and insightful responses from a PDF file. <br><br></p>',
     unsafe_allow_html=True,
 )
-
-
-def save_uploadedfile(uploaded_file):
-    with open(os.path.join("data", uploaded_file.name), "wb") as file:
-        file.write(uploaded_file.getbuffer())
-    return st.success("Saved File: {} to directory".format(uploaded_file.name))
 
 if "messages" not in st.session_state:
     st.session_state.messages = []
@@ -28,22 +25,24 @@ for message in st.session_state.messages:
         st.markdown(message["content"])
 
 if prompt := st.chat_input("How Can I Help You Today"):
-    # Add user message to chat history
+    t_prompt = translator.translate(prompt)
+    src_lang = t_prompt.src
+    
     st.session_state.messages.append({"role": "user", "content": prompt})
-    # Display user message in chat message container
+
     with st.chat_message("user"):
         st.markdown(prompt)
 
-    # Display assistant response in chat message container
     with st.chat_message("assistant"):
         with st.spinner('Thinking...'):
-            result = search(prompt)
+            result = search(t_prompt.text)
             message_placeholder = st.empty()
             full_response = result
-            message_placeholder.markdown(full_response)
-    # Add assistant response to chat history
+            fr = str(full_response)
+            t_response = translator.translate(fr, dest=src_lang)
+            bot_response = t_response.text
+            message_placeholder.markdown(bot_response)
     st.session_state.messages.append({"role": "assistant", "content": result})
-
 
 hide_st_style = """
     <style>
@@ -52,5 +51,4 @@ hide_st_style = """
     header {visibility: hidden;}
     </style>
 """
-
 st.markdown(hide_st_style, unsafe_allow_html=True)
